7938a2f627b13b04ec9bc2112ce19425131f2ec5
nadernmds <37673805+nadernmds@users.noreply.github.com>
2018-12-29 01:45:00 +0330

    asdasdasd

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //pep insists on changes
+            //pep go to Hell
             Console.WriteLine("pep");
         }
     }
